8fe4f7edb21c6448d8fb57838303c804832197c5
hit-dev <leonpwlwsk0713@gmail.com>
2023-05-24 08:51:58 +0900

    Initialize UI/Polymer

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,16 @@ const wss: WebSocketServer = new WebSocketServer({
     port:8080
 });
 
+const ws: WebSocket = new WebSocket('ws://localhost:8080');
+ws.on('open', () => {
+    const data: Object = {
+        command: "register",
+        content: "",
+    }
+    const json: string = JSON.stringify(data);
+    ws.send(json);
+});
+
 wss.on('connection', (ws: WebSocket) => {
     ws.on('message', (data: string)=>{
         const obj = JSON.parse(data);
