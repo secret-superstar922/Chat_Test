8faf7af40d38123f28ab2e4f277bb62fb7ba7b59
hit-dev <leonpwlwsk0713@gmail.com>
2023-05-30 17:39:50 +0900

    Change MongoDB setting

--- server/database/index.ts
+++ server/database/index.ts
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbName = process.env.DB_NAME;
-
 const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
 
 const conn:Promise<any> = mongoose.connect(connectionString, {});
